8c655a2c2f881e9fdfc9b7186da2f5b75bcf78d2
zhoutianli1 <2911479713@qq.com>
2022-10-08 11:47:32 +0800

    10

---
+++ src/main/java/com/zhou/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.zhou.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll();// 不拦截
+
+    }
+}
---
+++ src/main/java/com/zhou/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.zhou.controller;
+
+
+import com.zhou.mapper.UserMapper;
+import com.zhou.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+//dao->service->controller。这个项目没有什么要处理的逻辑，service和dao层一样，
+// 所以不需要写service层，去年的图书馆里系统就要service层
+@RestController
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+    //33集合20分
+    @RequestMapping("/userList")
+    public String list(Model model)
+    {
+        //跳转到user/list.html之前要先  拿到所有的user信息
+        List<User> userList=userMapper.queryUserList();
+        model.addAttribute("userList",userList);
+        System.out.println(userList);
+
+        return "user/list";
+        //跳转到用户列表user/list.html
+    }
+
+}
+
